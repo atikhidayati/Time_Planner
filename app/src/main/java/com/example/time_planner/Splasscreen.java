5f6b6aad5956ea5005833d2e8a6a219323d06d22
anitaputri <anitaaramadhani@gmail.com>
2023-06-14 21:22:14 +0700

    menambahkan splashscreen

---
+++ app/src/main/java/com/example/time_planner/Splasscreen.java
@@ -0,0 +1,33 @@
+package com.example.time_planner;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class Splasscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splasscreen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 4000L); //4000 L = 4 detik
+    }
+}
